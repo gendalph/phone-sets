acce46ad22c409bfa7e730a51a8562d6a1863d7e
Maxim Prohorenko <maxim.prohorenko@gmail.com>
2012-10-24 00:47:47 +0400

    data: convert price to int for sorting by value but not by string

--- proc/phone_table_model.cpp
+++ proc/phone_table_model.cpp
@@ -2,24 +2,42 @@
 
 QVariant phone_table_model::data (const QModelIndex &index, int role) const
 {
-  if (index.isValid () && role == Qt::DisplayRole)
+  if (!index.isValid ())
+    return QVariant ();
+
+  QList<gray_type::key_type> keys = gray_list.keys ();
+  int i_row = index.row ();
+  int j_col = index.column ();
+
+  if (i_row >= keys.size ())
+    return QVariant ();
+
+  j_col--;
+  if (j_col >= gray_list[keys[i_row]].size () || j_col < -1)
+    return QVariant ();
+
+  switch (role)
     {
-      QList<gray_type::key_type> keys = gray_list.keys ();
-      int i_row = index.row ();
-      int j_col = index.column ();
-
-      if (i_row >= keys.size ())
-        return QVariant ();
-
-      if (j_col == 0)
+    case Qt::TextAlignmentRole:
+      if (j_col == -1 || j_col == 11)
+        {
+          return Qt::AlignRight + Qt::AlignVCenter;
+        }
+      break;
+    case Qt::DisplayRole:
+      if (j_col == -1)
         return keys[i_row];
       else
         {
-          j_col--;
-          if (j_col >= gray_list[keys[i_row]].size ())
-            return QVariant ();
-          return gray_list[keys[i_row]][j_col];
+          if (j_col != 11)
+            return gray_list[keys[i_row]][j_col];
+          else
+            {
+              QString price = gray_list[keys[i_row]][j_col];
+              return price.remove (" ").toInt ();
+            }
         }
+      break;
     }
 
   return QVariant ();
